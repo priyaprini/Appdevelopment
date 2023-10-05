2ca7c4b01c7ec820fea8b685ca13520d23dcb866
priyaprini <priyadharshinikp19@gmail.com>
2023-10-05 11:00:56 +0530

    Day11

---
+++ Day 15/src/main/java/com/example/demo/config/JwtConstant.java
@@ -0,0 +1,13 @@
+package com.example.demo.config;
+
+
+public class JwtConstant {
+	
+	public static final String SECRET_KEY="kjhgtyedyesrdtfyhuiytiyuiyitu5ryguky7i6r5ydfhkgytrysdfh";
+	public static final String JWT_HEADER="Authorization";
+
+}
+
+
+
+
